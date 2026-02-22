c64c94b656dd2721ed7da2ab43c7e2da50838e67
sayanthsudhan <sayanth.sudhan17@gmail.com>
2026-02-22 17:55:25 +0530

    new names

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,7 +6,8 @@ const Team = () => {
         { id: 1, name: 'Rishnu lal' }, { id: 2, name: 'Hussain huzefa' }, { id: 3, name: 'Devapriya' }, { id: 4, name: 'Rida Waseem' },
         { id: 5, name: 'Adhi prajeesh' }, { id: 6, name: 'Lakshmi reji suresh' }, { id: 7, name: 'Aswandha RJ' }, { id: 8, name: 'sayanth p' },
         { id: 9, name: 'Muhammed Aswlah' }, { id: 10, name: 'Ananthanarayanan m' }, { id: 11, name: 'muhammed shabaz' }, { id: 12, name: 'Fathima P' },
-        { id: 13, name: 'Hemanth Sudhan' }, { id: 14, name: 'Srevan s' }, { id: 15, name: 'sadra sunil' }, { id: 16, name: 'Anvar sadth' }
+        { id: 13, name: 'Hemanth Sudhan' }, { id: 14, name: 'Srevan s' }, { id: 15, name: 'sadra sunil' }, { id: 16, name: 'Anvar sadth' },
+        { id: 17, name: 'Roshith krishna' }, { id: 18, name: 'Karthik C' }, { id: 19, name: 'MUhammed Sinan AP' },
     ];
 
     return (
